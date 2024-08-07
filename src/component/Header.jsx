ec0183c4fca5fccef1644b8d2cbd3e4f18540b79
Gu-Nayeon <listoss456@gmail.com>
2024-08-08 07:32:28 +0900

    header

---
+++ src/component/Header.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div style={{ width: 390, height: 109, position: 'relative', background: 'white' }}>
+                <div style={{ height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'black', fontSize: '17px', fontFamily: 'Pretendard', fontWeight: '500', lineHeight: '16px' }}>
+                    한끝봇
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Header;
